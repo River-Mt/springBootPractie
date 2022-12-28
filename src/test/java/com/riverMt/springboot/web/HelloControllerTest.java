01da08460ec15f81bad00243fa14eba3351f79af
rivermt <rivermt1998@gmail.com>
2022-12-28 14:30:04 +0900

    Test: add helloTest method

---
+++ src/test/java/com/riverMt/springboot/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.riverMt.springboot.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Hello 반환 테스트")
+    public void helloTest() throws Exception {
+        String hello = "hello!";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+
+}
