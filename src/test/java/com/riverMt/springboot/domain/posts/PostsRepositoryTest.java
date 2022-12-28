eac3113e3442477b76dddc7da8b367d119bc6415
rivermt <rivermt1998@gmail.com>
2022-12-28 15:10:27 +0900

    Feat: add PostsRequestDto

--- src/test/java/com/riverMt/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/riverMt/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,4 +1,47 @@
+package com.riverMt.springboot.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class PostsRepositoryTest {
-  
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After("cleanUp")
+    public void cleanUp() { postsRepository.deleteAll(); }
+
+    @Test
+    @DisplayName("게시글 저장 테스트")
+    public void savePostsTest() {
+        //given
+        String title = "title";
+        String content = "content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("riverMt")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
 }
