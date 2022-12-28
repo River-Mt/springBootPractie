fd657d5f7a48e3c4e8a8c4f1ab087db6f45970cc
rivermt <rivermt1998@gmail.com>
2022-12-28 14:49:22 +0900

    Test: add getHelloResponseDtoTest method

--- src/test/java/com/riverMt/springboot/web/HelloControllerTest.java
+++ src/test/java/com/riverMt/springboot/web/HelloControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -27,6 +28,21 @@ class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    @DisplayName("getHelloResponseDto 테스트")
+    public void getHelloResponseDtoTest() throws Exception {
+       String name = "test";
+       int amount = 1000;
+
+       mvc.perform(get("/hello/dto")
+               .param("name", name)
+               .param("amount", String.valueOf(amount)))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.name", is(name)))
+               .andExpect(jsonPath("$.amount",is(amount)));
+
     }
 
 }
